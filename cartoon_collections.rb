89076ac15d5e27d3a51883c47aab00647beb2826
Craig Ford <craig_ford@icloud.com>
2022-07-27 17:23:35 -0400

    commit tree b001f301c26769119a5a998403717a7ad7692900

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,39 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+def roll_call_dwarves(dwarf_arr)
+  dwarf_arr.map.with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  new_arr = planeteer_calls.map do |planet|
+    planet.capitalize + "!"
+  end
+  return new_arr 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words_array)
+
+  str = words_array.find do |word|
+    word.length > 4
+  end 
+  
+  if str == nil 
+    return false
+  else
+    return true
+  end   
+
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food_arr)
+
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  food_str = food_arr.find do |food|
+    cheese_types.include?food
+  end  
+
+  return food_str
 end
+
